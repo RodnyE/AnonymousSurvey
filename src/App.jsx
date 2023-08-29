a297077f73ce8a5df442919234a995fad9c0bba0
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-08-29 02:14:34 -0400

    feat (src/views): added GlobalContext.Provider component to use global states

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 
+import { useState } from 'react' 
+import { GlobalContext } from 'context'
+import LoginView from './views/Login'
+import SurveyView from './views/Survey'
+
 
 // Application 
 export default function App () {
+    const [currentViewName, setCurrentViewName] = useState("LoginView")
+    const [userName, setUserName] = useState("")
+    
+    
     return (
-      <div>
-         Anonymous Survey
-      </div>
+    <GlobalContext.Provider 
+        value={{
+            currentViewName,
+            setCurrentViewName,
+            userName,
+            setUserName,
+        }}
+    >
+        <LoginView  show={currentViewName === "LoginView"}/>
+        <SurveyView show={currentViewName === "SurveyView"}/> 
+    </GlobalContext.Provider>
     )
 }
