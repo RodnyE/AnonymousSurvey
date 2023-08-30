6381d72404971dc5e1f5264c8f21a8a052041a0f
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-08-30 02:04:19 -0400

    feat (src/logic): separated data objects

---
+++ src/logic/db.js
@@ -0,0 +1,24 @@
+
+import cloth1Img from "../assets/cloth-1.jpg"
+import cloth2Img from "../assets/cloth-2.jpg"
+
+export const listModels = [
+    { value: "Modelo 1", src: cloth1Img },
+    { value: "Modelo 2", src: cloth2Img },
+];
+
+export const listColors = [
+    { value: "Rojo", hex: "#FF0000" },
+    { value: "Verde", hex: "#00FF00" },
+    { value: "Azul", hex: "#0000FF" },
+    { value: "Amarillo", hex: "#FFFF00" },
+    { value: "Cian", hex: "#00FFFF" },
+    { value: "Magenta", hex: "#FF00FF" },
+    { value: "Negro", hex: "#000000" },
+    { value: "Blanco", hex: "#FFFFFF" },
+    { value: "Gris", hex: "#808080" },
+    { value: "Naranja", hex: "#FFA500" },
+    { value: "Rosa", hex: "#FFC0CB" },
+    { value: "Morado", hex: "#800080" },
+    { value: "Verde oscuro", hex: "#006400" },
+];
--- src/views/Survey.jsx
+++ src/views/Survey.jsx
@@ -2,33 +2,10 @@
 import { useState, useContext } from "react"
 import { View } from "ui"
 import { GlobalContext } from "context";
+
 import http from "utils/http"
 import stg from 'utils/storage'
-
-import cloth1Img from "../assets/cloth-1.jpg"
-import cloth2Img from "../assets/cloth-2.jpg"
-
-
-const listModels = [
-    { value: "Modelo 1", src: cloth1Img },
-    { value: "Modelo 2", src: cloth2Img },
-];
-
-const listColors = [
-    { value: "Rojo", hex: "#FF0000" },
-    { value: "Verde", hex: "#00FF00" },
-    { value: "Azul", hex: "#0000FF" },
-    { value: "Amarillo", hex: "#FFFF00" },
-    { value: "Cian", hex: "#00FFFF" },
-    { value: "Magenta", hex: "#FF00FF" },
-    { value: "Negro", hex: "#000000" },
-    { value: "Blanco", hex: "#FFFFFF" },
-    { value: "Gris", hex: "#808080" },
-    { value: "Naranja", hex: "#FFA500" },
-    { value: "Rosa", hex: "#FFC0CB" },
-    { value: "Morado", hex: "#800080" },
-    { value: "Verde oscuro", hex: "#006400" },
-];
+import {listModels, listColors} from "../logic/db"
 
 
 export default function SurveyView ({show}) {
